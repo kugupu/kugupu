3b9e3b0a4cb3646870e504cc2066ed6a0eac3395
Richard Gowers <richardjgowers@gmail.com>
2025-03-23 12:26:42 +0000

    fix versioneer

--- src/kugupu/_version.py
+++ src/kugupu/_version.py
@@ -53,7 +53,7 @@ def get_config() -> VersioneerConfig:
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "None"
-    cfg.versionfile_source = "kugupu/_version.py"
+    cfg.versionfile_source = "src/kugupu/_version.py"
     cfg.verbose = False
     return cfg
 
